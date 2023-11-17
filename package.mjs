9de40890d945afdc47338a7b09933803cf858552
Adam Avramov <adam@hack.bg>
2023-11-17 20:40:37 +0200

    fix(2.0.8): find own package.json correctly

--- package.mjs
+++ package.mjs
@@ -5,6 +5,6 @@ import { fileURLToPath } from 'node:url'
 import { resolve, relative, dirname } from 'node:path'
 import { readFileSync, statSync } from 'node:fs'
 import { Console, bold, colors } from '@hackbg/logs'
-const Package = JSON.parse(readFileSync(resolve(dirname(dirname(fileURLToPath(import.meta.url))), 'package.json'), 'utf8'))
+const Package = JSON.parse(readFileSync(resolve(dirname(fileURLToPath(import.meta.url)), 'package.json'), 'utf8'))
 const console = new Console(`@hackbg/ubik(${bold(Package.version)})`)
 export { Package as default, console, bold, colors }
